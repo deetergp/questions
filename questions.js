58df15bebd078564ecb3c1fb51c19a4485a050a2
Scott Deeter <scottd@knowclassic.com>
2014-03-18 21:55:22 -0400

    Removed "hello" template & isClient() code

--- questions.js
+++ questions.js
@@ -1,15 +1,5 @@
 Questions = new Meteor.Collection("Questions");
 
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return Session.get('greeting') || "Welcome to questions.";
-  };
 
-  Template.hello.events({
-    'click input': function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
 }
