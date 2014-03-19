37730552080bde39979671cd6a11179b93f6aaa8
Scott Deeter <scottd@knowclassic.com>
2014-03-18 22:02:42 -0400

    Added logic to add & list questions

--- questions.js
+++ questions.js
@@ -1,5 +1,17 @@
 Questions = new Meteor.Collection("Questions");
 
 if (Meteor.isClient) {
+  Template.questions.allQuestions = function () {
+    return Questions.find();
+  };
 
+  Template.questions.events({
+    'submit form': function (evt, templ) {
+      var question = templ.find('#questionText').value;
+      Questions.insert({
+        question: question,
+        score: 1
+      });
+    }
+  });
 }
